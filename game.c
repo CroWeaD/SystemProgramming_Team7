f7e427c7e409102a3336097702c43a14ce1d9f59
hjinshin <gudwls818@knu.ac.kr>
2023-05-24 14:12:05 +0900

    space travel salary

--- game.c
+++ game.c
@@ -189,7 +189,7 @@ void start_turn() {
         sendWhere2Travel(playerNum, currPlayer);
         /*RECV PACKET*/
         int travel = recvPack(playerNum, currPlayer);
-        travel -= SPACE_TRAVEL_SQR;
+        travel = travel > SPACE_TRAVEL_SQR ? travel - SPACE_TRAVEL_SQR : travel + TOTAL_SQR - SPACE_TRAVEL_SQR;
         curr_pos = move_player(travel);
         /*SEND PACKET*/
         sendTravelResult(playerNum, currPlayer, curr_pos, players[currPlayer].cash);
